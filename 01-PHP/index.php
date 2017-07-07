f71036206bd03be9f21e2fc1963b0ba8f579cebf
Justin Johnson <systemsevendesigns@gmail.com>
2017-07-07 05:30:24 -0400

    initial commit

---
+++ 01-PHP/index.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+*  Please complete the following function to take 3 arguments: protein, carbs, and fat
+*  Each argument may be passed in as a number or a string: 25, 25g, 25kg.
+*  If kg are passed in, there will need to be a conversion to grams
+*  The function should return an associative array with the following format
+*  The numeric value is the number of calories
+*  [
+*    'protein' => 25,
+*    'carbs' => 40,
+*    'fat' => 90,
+*    'calories' => 155
+*    ]
+*  For simplicity sakes: assume that 1g of protein is 4 calories, 1g of carbs is 4 calories, and 1g of fat is 9 calories
+**/
+
+
+    function macros_to_calories()
+    {
+
+    }
+
+
+
+?>
