270160b932c01c8ec36eba43bf17eeb2e181d405
BenBeehler <ben.beehler@gmail.com>
2017-07-09 18:53:41 -0400

    Updated PHP Piece

--- 01-PHP/index.php
+++ 01-PHP/index.php
@@ -16,11 +16,57 @@
 **/
 
 
-    function macros_to_calories()
+    function macros_to_calories($protein, $carbs, $fat)
     {
-
+		$protein = parse($protein); #Parse protein
+		$carbs = parse($carbs); #Parse carbs 
+		$fat = parse($fat); #Parse fat
+		
+		$calories = 0; #Init calories
+		
+		#Calories 
+		$calories += $protein * 4;
+		$calories += $carbs * 4;
+		$calories += $fat * 9;
+		
+		//Generate Statistics array
+		$statistics_array = 
+		[
+			'protein' => $protein,
+			'carbs' => $carbs,
+			'fat' => $fat,
+			'calories' => $calories
+		];
+		
+		return $statistics_array; //Finished!
     }
-
-
-
+	
+	function parse($macro) {
+		#This parses the macro for kg, g and generates data that needs to be returned
+		
+		$macro = (string) $macro;
+		
+		if(strpos($macro, 'kg') !== false) { //Kilogram format
+			#1 kg = 1000 grams
+			
+			$macro = (integer) 
+					str_replace('kg', ' ', $macro);
+			
+			$macro = (1000 * $macro); //Convert kilograms to grams
+			
+		} else if(strpos($macro, 'g')) { //Normal gram format
+			$macro = (integer) 
+					str_replace('g', ' ', $macro);
+		} else { //Normal Integer (should be, if not, person used function incorrectly)
+			#Noraml integer
+			$macro = (integer) $macro;
+		}
+		
+		return $macro;
+	}
+	
+	#Testing down here, seeing if it works
+	#foreach(macros_to_calories(5, '30kg', '10g') as $key => $value) {
+		#echo $key . " => " . $value . '(g) <br>';
+	#}
 ?>
